77aa75407da413efd32430a9faa299fc5da1a013
Tushar Dhakad <tushardhakad355@gmail.com>
2025-03-23 15:31:46 +0530

    first commit

---
+++ controllers/listing.js
@@ -0,0 +1,61 @@
+const Listing =require("../models/listing.js");
+module.exports.index=async (req,res)=>{
+  const allListings= await Listing.find({});
+  res.render("index.ejs",{allListings});
+}
+module.exports.renderNewForm=(req,res)=>{
+    res.render("new.ejs");
+}
+module.exports.showListing=async (req,res)=>{
+    let {id}= req.params;
+    const listing =await Listing.findById(id).populate({path:"reviews",populate:{path:"author",}}).populate("owner");
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
+  
+    res.render("show.ejs",{listing});
+} 
+module.exports.createListing=async (req,res,next)=>{
+     
+     let url =req.file.path;
+     let filename=req.file.filename;
+     
+    const newq= new Listing(req.body.listing);
+    newq.owner=req.user._id;
+    newq.image={url,filename};
+    await newq.save();
+    req.flash("success","New Listing Created!");
+    res.redirect("/listings");
+    }
+module.exports.destroyListing=async (req,res)=>{
+    let {id} =req.params;
+    let deleteListing= await Listing.findByIdAndDelete(id);
+    req.flash("success","Listing Deleted!");
+    res.redirect("/listings");
+}
+module.exports.renderEditForm=async (req,res)=>{
+    let {id}= req.params;
+    const listing =await Listing.findById(id);
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
+    let originalImageUrl= listing.image.url;
+    originalImageUrl= originalImageUrl.replace("/upload","/upload/w_250")
+    res.render("edit.ejs",{listing,originalImageUrl});
+}
+module.exports.updateListing=async (req,res)=>{
+
+  let {id}=req.params;
+ 
+ let listing= await Listing.findByIdAndUpdate(id,{...req.body.listing});
+ if(typeof req.file !== "undefined") {
+ let url =req.file.path;
+     let filename=req.file.filename;
+     listing.image={url,filename};
+ }
+     await listing.save();
+ req.flash("success","Listing Updated!");
+  res.redirect(`/listings/${id}`)
+}
